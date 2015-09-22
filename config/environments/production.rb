54ab5394584240f6e238ae8d4e213040514c89ee
abhimediratta <abhimediratta@gmail.com>
2015-09-22 19:19:40 +0530

    assets change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.assets.paths << Rails.root.join("vendor", "assets", "images")
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
